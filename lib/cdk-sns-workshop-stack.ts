4866923de2fa68636bb802c316c7b9b89e8d870c
ashizaki <ashizaki.ksk@gmail.com>
2022-12-19 11:59:47 +0900

    Step-1

--- lib/cdk-sns-workshop-stack.ts
+++ lib/cdk-sns-workshop-stack.ts
@@ -1,19 +1,58 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import {
+  IdentityPool,
+  UserPoolAuthenticationProvider,
+} from "@aws-cdk/aws-cognito-identitypool-alpha"
+import { CfnOutput, Stack, StackProps } from "aws-cdk-lib"
+import {
+  AccountRecovery,
+  UserPool,
+  UserPoolClient,
+  VerificationEmailStyle,
+} from "aws-cdk-lib/aws-cognito"
+import { Construct } from "constructs"
 
 export class CdkSnsWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    const queue = new sqs.Queue(this, 'CdkSnsWorkshopQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
+    const userPool = new UserPool(this, "UserPool", {
+      selfSignUpEnabled: true,
+      accountRecovery: AccountRecovery.PHONE_AND_EMAIL,
+      userVerification: {
+        emailStyle: VerificationEmailStyle.CODE,
+      },
+      autoVerify: {
+        email: true,
+      },
+      keepOriginal: { email: true },
+      standardAttributes: {
+        email: {
+          required: true,
+          mutable: true,
+        },
+      },
+    })
 
-    const topic = new sns.Topic(this, 'CdkSnsWorkshopTopic');
+    const userPoolClient = new UserPoolClient(this, "UserPoolClient", {
+      userPool,
+    })
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const identityPool = new IdentityPool(this, "IdentityPool", {
+      allowUnauthenticatedIdentities: true,
+      authenticationProviders: {
+        userPools: [
+          new UserPoolAuthenticationProvider({
+            userPool: userPool,
+            userPoolClient: userPoolClient,
+          }),
+        ],
+      },
+    })
+
+    new CfnOutput(this, "UserPoolId", { value: userPool.userPoolId })
+    new CfnOutput(this, "UserPoolClientId", { value: userPoolClient.userPoolClientId })
+    new CfnOutput(this, "IdentityPoolId", {
+      value: identityPool.identityPoolId,
+    })
   }
 }
